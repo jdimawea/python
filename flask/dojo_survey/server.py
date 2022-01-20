6e17842019ec3f037da3bb5446815ab58e1e1def
Jonathan Dima <dimawea@gmail.com>
2022-01-20 14:06:43 -0700

    Final Commit

--- flask/dojo_survey/server.py
+++ flask/dojo_survey/server.py
@@ -12,5 +12,19 @@ def index():
     return render_template("index.html")
 
 
+@app.route('/return', methods=['POST'])
+def goback():
+    return render_template('index.html')
+
+
+@app.route('/result', methods=['GET', 'POST'])
+def submit():
+    name = request.form['name']
+    location = request.form['location']
+    language = request.form['language']
+    comment = request.form['comment']
+    return render_template('result.html', name = name, location = location, language = language, comment = comment)
+
+
 if __name__=='__main__':
     app.run(debug=True)
