1e0788c72a40a5c437ab8101b20e65236d21ca2c
Jonathan Dima <dimawea@gmail.com>
2022-01-19 22:17:41 -0700

    update

--- flask/counter/server.py
+++ flask/counter/server.py
@@ -1,13 +1,34 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, session
 
 
 app = Flask(__name__)
 
 
+app.secret_key = 'VictoriaSecret'
+
+
 @app.route('/')
 def index():
-    return render_template("index.html")
+    if 'count' in session:
+        session['count'] = session.get('count') + 1
+    else:
+        session['count'] = 1
+    return render_template("index.html", count = session['count'])
 
 
-if __name__=="__main__":
+@app.route('/increment')
+def increment():
+    if 'count' in session:
+        session['count'] = session.get('count') + 1
+    else:
+        session['count'] = 1
+    return redirect('/')
+
+
+@app.route('/reset')
+def destroy_session():
+    session.clear()
+    return redirect('/')
+
+if __name__=='__main__':
     app.run(debug=True)
